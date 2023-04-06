62aac0ee71b5d2fb8e94d6532a43212418243380
jalaluddin <14-27477-2@student.aiub.edu>
2023-04-06 22:19:22 +0600

    Dashboard design changed

--- HospitalLeaveApplication/ViewModels/DashboardViewModel.cs
+++ HospitalLeaveApplication/ViewModels/DashboardViewModel.cs
@@ -1,16 +1,28 @@
 ﻿using HospitalLeaveApplication.Models;
 using HospitalLeaveApplication.Services;
 using MvvmHelpers;
+using MvvmHelpers.Commands;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 
 namespace HospitalLeaveApplication.ViewModels
 {
     public class DashboardViewModel : BaseViewModel
     {
+        public ICommand NavigateToLeaveApplicationsCommand { get; }
+        public DashboardViewModel()
+        {
+            NavigateToLeaveApplicationsCommand = new AsyncCommand(ExecuteNavigateToLeaveApplications);
+        }
+
+        private async Task ExecuteNavigateToLeaveApplications()
+        {
+            await Shell.Current.GoToAsync("//LeaveApplicationList");
+        }
         public void OnAppearing()
         {
             
