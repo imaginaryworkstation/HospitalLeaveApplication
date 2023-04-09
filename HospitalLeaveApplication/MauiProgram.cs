6e39dd3e773964291cc17665875191f88b27c1bb
jalal-uddin <jalal.imaginary@gmail.com>
2023-04-09 21:09:17 +0600

    Login page updated

--- HospitalLeaveApplication/MauiProgram.cs
+++ HospitalLeaveApplication/MauiProgram.cs
@@ -13,7 +13,10 @@ public static class MauiProgram
 			{
 				fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
 				fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
-			});
+                fonts.AddFont("fa-regular-400.ttf", "FASRegular");
+                fonts.AddFont("fa-brands-400.ttf", "FASBrand");
+                fonts.AddFont("fa-solid-900.ttf", "FASSolid");
+            });
 
 #if DEBUG
 		builder.Logging.AddDebug();
