6032f0b755fb3ea01c7ab3ffa3c1b43bc2b66242
jalal-uddin <jalal.imaginary@gmail.com>
2023-04-09 17:38:46 +0600

    Dashboard updated

--- HospitalLeaveApplication/ViewModels/DashboardViewModel.cs
+++ HospitalLeaveApplication/ViewModels/DashboardViewModel.cs
@@ -1,27 +1,41 @@
-﻿using HospitalLeaveApplication.Models;
-using HospitalLeaveApplication.Services;
-using MvvmHelpers;
+﻿using MvvmHelpers;
 using MvvmHelpers.Commands;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Input;
 
 namespace HospitalLeaveApplication.ViewModels
 {
     public class DashboardViewModel : BaseViewModel
     {
-        public ICommand NavigateToLeaveApplicationsCommand { get; }
+        public ICommand NavigateToNotificationListCommand { get; }
+        public ICommand NavigateToLeaveApplicationListCommand { get; }
+        public ICommand NavigateToNewLeaveApplicationCommand { get; }
+        public ICommand NavigateToProfileCommand { get; }
         public DashboardViewModel()
         {
-            NavigateToLeaveApplicationsCommand = new AsyncCommand(ExecuteNavigateToLeaveApplications);
+            NavigateToNotificationListCommand = new AsyncCommand(ExecuteNavigateToNotificationList);
+            NavigateToLeaveApplicationListCommand = new AsyncCommand(ExecuteNavigateToLeaveApplicationList);
+            NavigateToNewLeaveApplicationCommand = new AsyncCommand(ExecuteNavigateToNewLeaveApplication);
+            NavigateToProfileCommand = new AsyncCommand(ExecuteNavigateToProfile);
         }
 
-        private async Task ExecuteNavigateToLeaveApplications()
+        private async Task ExecuteNavigateToNotificationList()
+        {
+            await Shell.Current.GoToAsync("//NotificationListPage");
+        }
+
+        private async Task ExecuteNavigateToLeaveApplicationList()
         {
             await Shell.Current.GoToAsync("//LeaveApplicationListPage");
+        }
+
+        private async Task ExecuteNavigateToNewLeaveApplication()
+        {
+            await Shell.Current.GoToAsync("//LeaveApplicationPage");
+        }
+
+        private async Task ExecuteNavigateToProfile()
+        {
+            await Shell.Current.GoToAsync("//ProfilePage");
         }
         public void OnAppearing()
         {
